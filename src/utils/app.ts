9e43a990f4a638fd6b8edb5354258d6027c79613
Jan Libal <libalj@seznam.cz>
2024-03-14 17:52:21 +0100

    Add Koa related packages and implementation

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,5 +1,35 @@
 import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import koaBody from 'koa-body'
+import requestId from 'koa-requestid'
+import koaCompress from 'koa-compress'
+import koaHelmet  from 'koa-helmet'
+import cors from '@koa/cors'
+import koaLogger from 'koa-logger'
+import errorHandler from '../middleware/errorHandler'
+
+import config from '../config'
 
 const app = new Koa()
+
+app.use(errorHandler)
+app.use(requestId())
+app.use(koaCompress())
+app.use(koaBody())
+app.use(koaLogger())
+app.use(cors({
+  origin: config.server.cors.origin,
+  allowMethods: config.server.cors.allowedMethods,
+  allowHeaders: config.server.cors.allowedHeaders,
+  exposeHeaders: config.server.cors.exposeHeaders
+}))
+
+app.use(
+  bodyParser({
+    enableTypes: ["json"],
+    jsonLimit: '10mb'
+  }),
+)
+app.use(koaHelmet())
 
 export default app
