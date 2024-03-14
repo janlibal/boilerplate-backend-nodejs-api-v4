c339387b8aec190c6f0fd4ce4fcaab5ca8994618
Jan Libal <libalj@seznam.cz>
2024-03-14 17:56:09 +0100

    Add server launch

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import logger from "./utils/logger"
+import config from './config'
+import logger from './utils/logger'
+import createServer from './utils/server'
 
-export async function helloWorld() {
-    logger.info('Logging started')
-    const data= 'Hello world!'
-    return data
+export async function server(){
+
+  let server: any
+  const srvPort = config.server.port
+
+
+  try {
+
+    server = createServer
+    logger.info(`Server is listening on ${srvPort}. `)
+
+  } catch (err:any) {
+
+    process.exitCode = 1
+    logger.fatal('FATAL ERROR WHILE STARTING SERVER!')
+
+  } finally {
+    if (!server){
+      logger.debug("Closing server...")
+      await server.close()
+      logger.debug("Server closed")
+    }
+
+  }
+
 }
 
-helloWorld()
+server()
