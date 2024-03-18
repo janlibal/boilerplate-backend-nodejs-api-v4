1d24c564cc1c684d3d041c1280d1c44e68e2719c
Jan Libal <libalj@seznam.cz>
2024-03-18 17:55:34 +0100

    Update login() with generateAccessToken()

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -26,20 +26,14 @@ async function login(input: IUser) {
   if (!verified) {
     logger.info('Unauthorized')
     throw new errors.UnauthorizedError('Invalid email or password')
-}
-
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
-
-  const fakeUser = {
-      email: 'joe.doe@joedoe.com',
-      token
   }
-  
 
-  logger.info({email: fakeUser.email, token: token}, 'login finished')
+  const token = await crypto.generateAccessToken(user.id)
+
+  logger.info({email: user.email, token: token}, 'login finished')
 
   return {
-      email: fakeUser.email,
+      email: user.email,
       token
     }
   }
