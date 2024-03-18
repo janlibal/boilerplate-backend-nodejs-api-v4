445a31fb1a5cc403bf339b4a33bef6b4df2b13c5
Jan Libal <libalj@seznam.cz>
2024-03-18 12:02:35 +0100

    Add /api/v1/user route test

---
+++ test/user.test.ts
@@ -0,0 +1,31 @@
+import supertest from 'supertest'
+import app from '../src/utils/app'
+import { knex } from '../src/database'
+
+
+const server = app.listen()
+
+afterAll(async () => {
+    await server.close()
+})
+
+
+describe('POST /api/v1/user', () => {
+
+  beforeEach(async() => {
+      return await knex.migrate.rollback()
+      .then(async () => {return await knex.migrate.latest()})
+    })
+
+    afterEach(async () => {
+      return await knex.migrate.rollback()
+    })
+
+    it('Should return 200 to make sure the route works', async () => {
+      const request = supertest(server)
+      const res = await request
+      .post(`/api/v1/user`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+  })
+})
