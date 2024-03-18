c0819cdcd77a554597b813f309bdd9b49fa679b7
Jan Libal <libalj@seznam.cz>
2024-03-18 15:48:44 +0100

    Update signIn() to retrieve input data

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -24,6 +24,12 @@ export async function signUp(ctx:IContext){
 
 export async function signIn(ctx:IContext){
 
+    const input:IUser = {
+        email: ctx.request.body.email,
+        name: ctx.request.body.name,
+        password: ctx.request.body.password
+    }
+
     const fakeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
 
     const fakeUser = {
