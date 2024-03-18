aa20b9ad6a93bc690784c66c830b1e80f1c06fa2
Jan Libal <libalj@seznam.cz>
2024-03-18 12:05:24 +0100

    Add /api/v1/user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import Router from 'koa-router'
 import testRoutes from './testRoutes'
+import userRoutes from './userRoutes'
 
 const router = new Router()
-
+const api = new Router()
 const test = new Router()
 
 test.use(testRoutes)
+api.use(userRoutes)
 
 router.use('', test.routes())
+router.use('/api/v1', api.routes())
 
 export default router
