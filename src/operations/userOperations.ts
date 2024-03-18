c45a9417c1194462cef613604b6236dc879e8b88
Jan Libal <libalj@seznam.cz>
2024-03-18 17:49:27 +0100

    Update login() with comparePasswords()

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -21,6 +21,8 @@ async function login(input: IUser) {
       throw new errors.UnauthorizedError('Invalid email or password')
   }
 
+  const verified = await crypto.comparePasswords(data.password, user.password)
+
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
 
   const fakeUser = {
