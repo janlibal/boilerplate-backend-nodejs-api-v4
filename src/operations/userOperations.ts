073385a57d1ee5f1c38fe79f7f201408abee7b1e
Jan Libal <libalj@seznam.cz>
2024-03-18 13:21:28 +0100

    Update create() to retrieve coming data

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -5,6 +5,12 @@ async function create(input: IUser) {
     
     logger.info('create user started')
 
+    const data = {
+      email: input.email,
+      name: input.name,
+      password: input.password
+  }
+
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
     
     const fakeUser = {
