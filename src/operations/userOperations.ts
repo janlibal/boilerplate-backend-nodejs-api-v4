760b0126b99e382995e5fb9e93f2d31c42cb3e2c
Jan Libal <libalj@seznam.cz>
2024-03-18 17:42:23 +0100

    Add login in userOperations

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -4,6 +4,28 @@ import crypto from "../utils/crypto"
 import userRepository from "../repositories/userRepository"
 import * as errors from '../utils/errors'
 
+
+async function login(input: IUser) {
+
+  logger.info('login started')
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
+
+  const fakeUser = {
+      email: 'joe.doe@joedoe.com',
+      token
+  }
+  
+
+  logger.info({email: fakeUser.email, token: token}, 'login finished')
+
+  return {
+      email: fakeUser.email,
+      token
+    }
+  }
+
+
 async function create(input: IUser) {
     
     logger.info('create user started')
@@ -35,5 +57,6 @@ async function create(input: IUser) {
     
 }
 export default { 
-    create
+    create,
+    login
 }
