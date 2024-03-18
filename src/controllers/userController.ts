be0afded4be6547d071a0c7213b56543caf854b0
Jan Libal <libalj@seznam.cz>
2024-03-18 13:14:50 +0100

    Update singUp() with input data validator

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { IContext } from "../interfaces/IContext"
 import { IUser } from "../interfaces/IUser"
-
-
+import validate from "../validations"
+import schema from '../validations/schemas/userSchema'
 
 export async function signUp(ctx:IContext){
 
@@ -10,6 +10,8 @@ export async function signUp(ctx:IContext){
         name: ctx.request.body.name,
         password: ctx.request.body.password
     }
+
+    await validate(schema.signUp, input)
 
     const fakeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
     
