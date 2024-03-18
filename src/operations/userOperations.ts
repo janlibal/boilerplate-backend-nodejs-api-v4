b52a39a71a49453ea0d306c0850b09ae50dfd04e
Jan Libal <libalj@seznam.cz>
2024-03-18 13:37:01 +0100

    Update create() with saveUser()

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -21,6 +21,9 @@ async function create(input: IUser) {
       throw new errors.ResourceAlreadyExists('User already registered')
     }
 
+    let createdUser: any
+    createdUser = await userRepository.saveUser(data)
+
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
     
     const fakeUser = {
