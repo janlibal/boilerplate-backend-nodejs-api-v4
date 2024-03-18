0e272617a24488d11586151af4fd3bc6e3ed40ae
Jan Libal <libalj@seznam.cz>
2024-03-18 13:33:52 +0100

    Update create() with findByEmail()

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -1,6 +1,8 @@
 import { IUser } from "../interfaces/IUser"
 import logger from "../utils/logger"
 import crypto from "../utils/crypto"
+import userRepository from "../repositories/userRepository"
+import * as errors from '../utils/errors'
 
 async function create(input: IUser) {
     
@@ -10,7 +12,14 @@ async function create(input: IUser) {
       email: input.email,
       name: input.name,
       password: await crypto.hashPassword(input.password),
-  }
+    }
+
+    const user = await userRepository.findByEmail(data.email)
+        
+    if (user) {
+      logger.info('Resource already exists')
+      throw new errors.ResourceAlreadyExists('User already registered')
+    }
 
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZDU0M2E0YS05ZTkwLTQ1MjAtYTljNS1mMTE2YjgyMTgxOTIiLCJpYXQiOjE3MDk4MTk1MjIsImV4cCI6MTcwOTgyOTUyMiwiaXNzIjoiQ09NLkpBTkxJQkFMLmRldmVsb3BtZW50In0.12jmcKAa20PgC_l4DMYRGBdO8lzbWL1cj2xTvp7lhOk'
     
