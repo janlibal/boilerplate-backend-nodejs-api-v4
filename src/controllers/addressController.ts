1abc92fa4a493f7b7a37112b0802c4aad3919e6b
Jan Libal <libalj@seznam.cz>
2024-03-19 17:42:30 +0100

    Update address() to retrieve input data

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -2,6 +2,15 @@ import { IContext } from "../interfaces/IContext";
 
 export async function address(ctx:IContext){
 
+    const userId = ctx.state.userId
+
+    const body = {
+        firstName: ctx.request.body.firstName,
+        lastName: ctx.request.body.lastName,
+        phoneNo: ctx.request.body.phoneNo,
+        address: ctx.request.body.address,
+    }
+
     const _writeTime = {
         _seconds: 10000,
         _nanoseconds: 10000
