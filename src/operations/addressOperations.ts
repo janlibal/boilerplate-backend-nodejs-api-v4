3e87834d535ae04a54dc0ffd07fcb2638346603c
Jan Libal <libalj@seznam.cz>
2024-03-19 17:53:39 +0100

    Update create() to retrieve incoming data

--- src/operations/addressOperations.ts
+++ src/operations/addressOperations.ts
@@ -5,6 +5,20 @@ async function create(input: IContact) {
     
     logger.info('create address started')
 
+    const fullName = input.lastName + ', ' + input.firstName
+     
+    const contactData: IContactData = {
+        firstName: input.firstName,
+        lastName: input.lastName,
+        phoneNo: input.phoneNo,
+        address: input.address,
+    }
+
+    const userData: IUserData = {
+        fullName: fullName,
+        userId: input.userId.toString(),
+    }
+
     const _writeTime = {
         _seconds: 10000,
         _nanoseconds: 10000
