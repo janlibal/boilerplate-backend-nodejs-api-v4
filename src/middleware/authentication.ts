45ceb4a35d11ec7baf552bae1795e4362e62db81
Jan Libal <libalj@seznam.cz>
2024-03-19 14:56:21 +0100

    Update authentication() with authorization header check

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -4,6 +4,11 @@ import logger from "../utils/logger"
 
 export async function authenticate(ctx:IContext, next:Next) {
 
+    if (!ctx.header.authorization) {
+        logger.info('No authorization defined')
+        throw new Error('No authorization defined')
+      }
+
     return next()
   }
   
