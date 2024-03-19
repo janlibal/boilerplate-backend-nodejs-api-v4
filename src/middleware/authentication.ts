9d5fe53610e12def63741502aa39ba153f2bb273
Jan Libal <libalj@seznam.cz>
2024-03-19 15:29:02 +0100

    Add getAuthPayload() in authentication middleware

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,13 +1,50 @@
 import { Next } from "koa"
 import { IContext } from "../interfaces/IContext"
 import logger from "../utils/logger"
+import * as errors from '../utils/errors'
+
+
+async function parseHeader(hdrValue:string) {
+
+    if (!hdrValue || typeof hdrValue !== 'string') {
+      return null
+    }
+  
+    const matches = hdrValue.match(/(\S+)\s+(\S+)/u)
+      
+    const data =  matches && {
+      scheme: matches[0],
+      value: matches[1],
+    }
+  
+    return data
+  }
+
+
+
+async function getAuthPayload(token:string) {
+  
+    if (!token) {
+      return null
+    }
+        
+    return true
+     
+   }
 
 export async function authenticate(ctx:IContext, next:Next) {
 
     if (!ctx.header.authorization) {
         logger.info('No authorization defined')
         throw new Error('No authorization defined')
-      }
+    }
+
+    const jwtToken = ctx.header.authorization!
+
+    const data = await getAuthPayload(jwtToken)
+    if (!data) {
+        throw new errors.InvalidToken()
+    }
 
     return next()
   }
