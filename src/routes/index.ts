9a872b48cc924839b0f47ca47703a70b0be22bd4
Jan Libal <libalj@seznam.cz>
2024-03-19 13:49:31 +0100

    Add /api/v1/address route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import Router from 'koa-router'
 import testRoutes from './testRoutes'
 import userRoutes from './userRoutes'
+import addressRoutes from './addressRoutes'
 
 const router = new Router()
 const api = new Router()
@@ -8,6 +9,7 @@ const test = new Router()
 
 test.use(testRoutes)
 api.use(userRoutes)
+api.use(addressRoutes)
 
 router.use('', test.routes())
 router.use('/api/v1', api.routes())
