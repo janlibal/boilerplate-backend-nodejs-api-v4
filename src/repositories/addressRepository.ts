f78910ccf5bae6ef96a078b3de706239171605d8
Jan Libal <libalj@seznam.cz>
2024-03-19 17:59:47 +0100

    Update save() to insert data into firestore db

--- src/repositories/addressRepository.ts
+++ src/repositories/addressRepository.ts
@@ -1,16 +1,15 @@
+import credentials from '../config/firestore/key.json'
+import admin from 'firebase-admin'
 import { IContactData, IUserData } from '../interfaces/IContact'
 
+admin.initializeApp({
+    credential: admin.credential.cert(credentials as admin.ServiceAccount),
+})
+
+const _db = admin.firestore()
+
 async function save(contactData: IContactData, userData:IUserData) {
-    const _writeTime = {
-        _seconds: 10000,
-        _nanoseconds: 10000
-    }   
-
-    const contact = {
-        _writeTime
-    }
-
-    return
+    return await _db.collection(userData.userId).doc(userData.fullName).set(contactData)
 }
 
 export default {
