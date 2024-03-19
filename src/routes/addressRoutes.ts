b14f8ecffa468ab4549020c6d368915b906690ab
Jan Libal <libalj@seznam.cz>
2024-03-19 14:54:38 +0100

    Add authenicate middleware to addressRoute

--- src/routes/addressRoutes.ts
+++ src/routes/addressRoutes.ts
@@ -1,8 +1,9 @@
 import Router from "koa-router"
 import { address } from "../controllers/addressController"
+import { authenticate } from "../middleware/authentication"
 
 const router = new Router()
 
-router.post('/address', address)
+router.post('/address', authenticate, address)
 
 export default router.routes()
