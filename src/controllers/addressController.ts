1cc314d2233d6b300729f3cc24016a5420bc2949
Jan Libal <libalj@seznam.cz>
2024-03-19 13:51:56 +0100

    Add address() in addressController

---
+++ src/controllers/addressController.ts
@@ -0,0 +1,7 @@
+import { IContext } from "../interfaces/IContext";
+
+export async function address(ctx:IContext){
+
+    ctx.status = 200
+
+}
--- src/routes/addressRoutes.ts
+++ src/routes/addressRoutes.ts
@@ -1,7 +1,8 @@
 import Router from "koa-router"
+import { address } from "../controllers/addressController"
 
 const router = new Router()
 
-router.post('/address')
+router.post('/address', address)
 
 export default router.routes()
